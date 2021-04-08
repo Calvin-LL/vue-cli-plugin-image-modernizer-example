cd6b885c4767d9f07aec4cb874f6caa6a91617fb
Calvin Liang <me@calvin.sh>
2021-04-07 20:39:35 -0700

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img
+      style="width: 50vw"
+      src="@/assets/Macaca_nigra_self-portrait_large.jpg"
+      modernize
+    />
+    <img
+      style="width: 50vw"
+      src="@/assets/Simple_Periodic_Table_Chart-en.png"
+      modernize
+    />
+    <img
+      style="width: 50vw"
+      src="@/assets/Macaca_nigra_self-portrait_large.jpg"
+      modernize="onlyUseImg"
+    />
+    <img
+      style="width: 50vw"
+      src="@/assets/Simple_Periodic_Table_Chart-en.png"
+      modernize="onlyUseImg"
+    />
+  </div>
+</template>
